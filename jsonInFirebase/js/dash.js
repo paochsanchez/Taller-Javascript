c5d4a82502bf0266a17ec62a2466d6dc3c4fcc2a
Paolanc <apaolachsanchez@gmail.com>
2021-06-02 07:49:53 -0600

    Promises

--- jsonInFirebase/js/dash.js
+++ jsonInFirebase/js/dash.js
@@ -2,140 +2,141 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var d_1 = document.getElementById('Doughnut_1').getContext('2d');
 var d_2 = document.getElementById('Doughnut_2').getContext('2d');
 
-let data_ = JSON.parse(localStorage.getItem("data"));
+let infoR;
+let values = [];
+
 let res = (a, b) => (a - b) >= 0 ? (a - b).toLocaleString('en-emodeng') : '-₡' + ((-1 * (a - b)).toLocaleString('en-emodeng'));
 
-function calculate() {
-    console.log(data_);
+function calculate(data_) {
 
-    let budget = data_[0].infoResult.data[0].budget;
-    let pMonth = data_[0].infoResult.data[0].pastMonthSale;
-    let pYear = data_[0].infoResult.data[0].pastYearSale;
+    infoR = data_[0].infoResult.data[0];
+
+    let budget = infoR.budget;
+    let pMonth = infoR.pastMonthSale;
+    let pYear = infoR.pastYearSale;
     let wW = 0;
     let saleV = 0;
-    let values = [];
 
     data_[0].weekResult.data.forEach(todo => {
         wW += (todo.weekWeight) / 100;
         saleV += todo.sale;
         values.push([lazyround(budget * wW),
-            lazyround(saleV),
-            lazyround(pYear * wW),
-            lazyround(pMonth * wW),
+        lazyround(saleV),
+        lazyround(pYear * wW),
+        lazyround(pMonth * wW),
         ])
     })
-    console.log(values);
-    iniciar(values);
+    console.log('listo');
 }
 
-calculate();
 
+function iniciar(data_) {
 
-function iniciar(values) {
-
-    let sale = (data_[0].infoResult.data[0].sale);
-    let month = data_[0].infoResult.data[0].monthAdvance;
-    let budget = (data_[0].infoResult.data[0].budget);
-    let cot = data_[0].infoResult.data[0].quotations;
-    let salesOrd = data_[0].infoResult.data[0].salesOrders;
-    let salesY = data_[0].infoResult.data[0].yearSale;
-    let budY = data_[0].infoResult.data[0].yearBudget;
-    let fact = data_[0].infoResult.data[0].invoices;
-    let cred = data_[0].infoResult.data[0].creditNotes;
+    let sale = infoR.sale;
+    let month = infoR.monthAdvance;
+    let budget = infoR.budget;
+    let cot = infoR.quotations;
+    let salesOrd = infoR.salesOrders;
+    let salesY = infoR.yearSale;
+    let budY = infoR.yearBudget;
+    let fact = infoR.invoices;
+    let cred = infoR.creditNotes;
 
     let dif = (res(sale, budget)).toLocaleString('en-emodeng');
-    let difY = (budY>0) ? ((salesY / budY)*100).toFixed(2) : 0;
-    console.log(difY);
+    let difY = (budY > 0) ? ((salesY / budY) * 100).toFixed(2) : 0;
     let proyected = (month > 0) ? (sale * 100 / month) : 0;
     let percent = (((budget > 0) ? (proyected / budget) : 0) * 100).toFixed(2);
     let cumpl = budget > 0 ? (sale / budget) * 100 : 100;
 
-    document.getElementById('val_Cumpl').innerHTML = '<div class="col">Venta Actual<br >₡' + sale.toLocaleString('en-emodeng') + '</div><div class="col">Meta Actual<br>₡' + budget.toLocaleString('en-emodeng') + '</div><div class="col">Diferencia<br>' + dif + '</div>';
-
-    document.getElementById("val_Cumpl2").innerHTML = ( cot != 0 ? '₡' + (cot).toLocaleString('en-emodeng') : '-');
-    document.getElementById("val_Cumpl1").innerHTML = ( salesOrd != 0 ? '₡' + (salesOrd).toLocaleString('en-emodeng') : '-');
-
-    document.getElementById("row_").innerHTML = ( salesY != 0 ? '₡' + (salesY).toLocaleString('en-emodeng') : '-');
-    document.getElementById("cump_").innerHTML = difY+'%' ;
-    document.getElementById("fact_").innerHTML = ( fact != 0 ? '₡' + (fact).toLocaleString('en-emodeng') : '-');
-    document.getElementById("dev_").innerHTML = ( (res(cred,0)).toLocaleString('en-emodeng'));
+    document.getElementById('val_Cumpl').innerHTML = '<div class="col"><div class="tls">Venta Actual</div><hr>₡' + sale.toLocaleString('en-emodeng') + '</div><div class="col"><div class="tls">Meta Actual</div><hr>₡' + budget.toLocaleString('en-emodeng') + '</div><div class="col"><div class="tls">Diferencia</div><hr>' + dif + '</div>';
+    document.getElementById("val_Cumpl2").innerHTML = (cot != 0 ? '₡' + (cot).toLocaleString('en-emodeng') : '-');
+    document.getElementById("val_Cumpl1").innerHTML = (salesOrd != 0 ? '₡' + (salesOrd).toLocaleString('en-emodeng') : '-');
+    document.getElementById("row_").innerHTML = (salesY != 0 ? '₡' + (salesY).toLocaleString('en-emodeng') : '-');
+    document.getElementById("cump_").innerHTML = difY + '%';
+    document.getElementById("fact_").innerHTML = (fact != 0 ? '₡' + (fact).toLocaleString('en-emodeng') : '-');
+    document.getElementById("dev_").innerHTML = ((res(cred, 0)).toLocaleString('en-emodeng'));
+    document.getElementById("navbar-brand-l").innerHTML = infoR.slpName;
 
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4', 'Semana 5', 'Semana 6'],
             datasets: [{
-                    label: 'Meta',
-                    data: [values[0][0], values[1][0], values[2][0], values[3][0], values[4][0], values[5][0]],
-                    backgroundColor: 'rgba(255, 159, 64, 1)',
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 1,
-                    
-                },
-                {
-                    label: 'Venta',
-                    data: [values[0][1], values[1][1], values[2][1], values[3][1], values[4][1], values[5][1]],
-                    backgroundColor: 'rgba(153, 102, 255, 1)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Año Anterior',
-                    data: [values[0][2], values[1][2], values[2][2], values[3][2], values[4][2], values[5][2]],
-                    backgroundColor: 'rgba(54, 162, 235, 1)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Mes Anterior',
-                    data: [values[0][3], values[1][3], values[2][3], values[3][3], values[4][3], values[5][3]],
-                    backgroundColor: 'rgba(255, 99, 132, 1)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }
+                label: 'Meta',
+                data: [values[0][0], values[1][0], values[2][0], values[3][0], values[4][0], values[5][0]],
+                backgroundColor: 'rgba(255, 159, 64, 1)',
+                borderColor: 'rgba(255, 159, 64, 1)',
+                borderWidth: 1,
+
+            },
+            {
+                label: 'Venta',
+                data: [values[0][1], values[1][1], values[2][1], values[3][1], values[4][1], values[5][1]],
+                backgroundColor: 'rgba(153, 102, 255, 1)',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Año Anterior',
+                data: [values[0][2], values[1][2], values[2][2], values[3][2], values[4][2], values[5][2]],
+                backgroundColor: 'rgba(54, 162, 235, 1)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Mes Anterior',
+                data: [values[0][3], values[1][3], values[2][3], values[3][3], values[4][3], values[5][3]],
+                backgroundColor: 'rgba(255, 99, 132, 1)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }
             ]
         },
         options: {
             responsive: true,
             maintainAspectRatio: false,
-            aspectRatio: 1, 
+            aspectRatio: 1,
             scales: {
                 y: {
                     grid: {
-                      color: '#00000081',
-                      borderColor: '#00000081',
-                      tickColor: '#00000081'
-                    },ticks: {
-                        color: 'black'
-                        
-                    }
-                  },
-                x:{
-                    grid:{ display: false},
+                        color: '#00000081',
+                        borderColor: '#00000081',
+                        tickColor: '#00000081'
+                    },
                     ticks: {
                         color: 'black'
-                        
+
                     }
-                    
+                },
+                x: {
+                    grid: {
+                        display: false
+                    },
+                    ticks: {
+                        color: 'black'
+
+                    }
+
                 }
-                
+
             },
             plugins: {
-            legend: { position:'top',
-                display: true,
-                labels: {
-                    color: 'black',
-                    font: {
-                        size: 14
+                legend: {
+                    position: 'top',
+                    display: true,
+                    labels: {
+                        color: 'black',
+                        font: {
+                            size: 14
+                        }
                     }
                 }
             }
-        }
-        
+
         }
 
     });
-    
+
     var Chart_1 = new Chart(d_1, {
         type: 'doughnut',
         data: {
@@ -148,9 +149,9 @@ function iniciar(values) {
             }]
         },
         options: {
-            maintainAspectRatio : false,
-            responsive:true,
-            aspectRatio: 1, 
+            maintainAspectRatio: false,
+            responsive: true,
+            aspectRatio: 1,
 
             tooltips: {
                 enabled: false
@@ -169,7 +170,7 @@ function iniciar(values) {
                 ctx.font = fontSize + "em sans-serif";
                 ctx.textBaseline = "middle";
 
-                var text = "75%",
+                var text = cumpl.toFixed(2) + '%',
                     textX = Math.round((width - ctx.measureText(text).width) / 2),
                     textY = height / 2;
 
@@ -191,17 +192,17 @@ function iniciar(values) {
             }]
         },
         options: {
-            maintainAspectRatio : false,
-            responsive:true,
+            maintainAspectRatio: false,
+            responsive: true,
             tooltips: {
                 enabled: false
             },
-            aspectRatio: 1, 
+            aspectRatio: 1,
             cutout: '90%'
         },
         plugins: [{
             id: 'text',
-            
+
             beforeDraw: function (chart, a, b) {
                 var width = chart.width,
                     height = chart.height,
@@ -212,13 +213,13 @@ function iniciar(values) {
                 ctx.font = fontSize + "em sans-serif";
                 ctx.textBaseline = "middle";
 
-                var text = "75%",
+                var text = percent + '%',
                     textX = Math.round((width - ctx.measureText(text).width) / 2),
                     textY = height / 2;
 
                 ctx.fillText(text, textX, textY);
                 ctx.save();
-                
+
             }
         }]
     });
@@ -229,12 +230,44 @@ function lazyround(num) {
 
 };
 
-$(document).ready(function() {
+
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        const data_ = JSON.parse(localStorage.getItem("data"));
+        setTimeout(
+            () => {
+                calculate(data_);
+                resolve(data_);
+            },
+            1000
+        );
+    });
+}
+
+
+async function startData() {
+    await getData()
+        .then(data_ => iniciar(data_))
+        .catch(error => {
+            console.log(error)
+        });
+}
+
+startData();
+
+$(document).ready(function () {
+
     $("#dough_1-2").css({
-      'width': ($("#dough_1").width() + 'px')
+        'width': ($("#dough_1").width() + 'px')
     });
     $("#dough_2-2").css({
         'width': ($("#dough_2").width() + 'px')
-      });
-  });
-  
+    });
+});
+
+$(window).on("load", function () {
+    $(".loader-container").fadeOut(1000);
+});
+
+
--- jsonInFirebase/js/functions.js
+++ jsonInFirebase/js/functions.js
@@ -2,46 +2,87 @@
 const database = firebase.database();
 // referencia a la collection test_col para utilizar las funciones sobre esta colección
 const rootRef = database.ref('/');
-//
+
 let jsonData = [];
 
-let res = (a,b) => (a-b) >= 0 ? (a-b).toLocaleString('en-emodeng') : '-₡' + ((-1*(a-b)).toLocaleString('en-emodeng'));
+let res = (a, b) => (a - b) >= 0 ? (a - b).toLocaleString('en-emodeng') : '-₡' + ((-1 * (a - b)).toLocaleString('en-emodeng'));
 
-let cumpl = (a,b) => b>0 ? '%'+ Math.round((a*100)/b ): '-';
+let cumpl = (a, b) => b > 0 ?  Math.round((a * 100) / b) : 100;
 
-function showData (){
-    let tableHtml = '';
-    let i = 0;
-    jsonData.forEach(todo => {
-        todo["id"] = i++;
-        tableHtml += '<tr todoId = "' + todo.id + '">';
-        tableHtml += '<td class = "text-truncate text-center"' + '> ' + todo.infoResult.data[0].slpName + '</td>';
-        tableHtml += '<td class = "text-truncate text-center"' + '>₡' + (todo.infoResult.data[0].sale).toLocaleString('en-emodeng') + '</td>';
-        tableHtml += '<td class = "text-truncate text-center"' + '>₡' + (todo.infoResult.data[0].budget).toLocaleString('en-emodeng') + '</td>';
-        tableHtml += '<td class = "text-truncate text-center"' + '>' + res(todo.infoResult.data[0].sale,todo.infoResult.data[0].budget) + '</td>';
-        tableHtml += '<td class = "text-truncate text-center"' + '>' + cumpl(todo.infoResult.data[0].sale ,todo.infoResult.data[0].budget) + '</td>';
-        tableHtml += '<td class = "text-truncate text-center"' + '><a class="btn btn-outline-success mx-2" href="dashboard.html"  onClick="actualClient('+todo.id+')">Exito</a></td>';
-        tableHtml += '</tr>';
-       
-    });
-    document.getElementById("tbodytable1ID").innerHTML = tableHtml;
-    console.log(jsonData);
-    console.log(document.getElementById('demo'));
-    $('#table_id').DataTable({
-        "aaSorting": [],
-        "paging" : true,
-        "lengthMenu": [3, 5, 9],
-        "pageLength" : 3
-      });
+let table = '<div class="card" id="tabla_"> <div class="card-header"> <h4 class="card-title text-center" style="font-size: 30px;">Lista de Vendedores</h4> </div><div class="card-content collapse show"><div class="card-body card-dashboard"><div class="table-responsive"><table class="table display nowrap table-striped table-bordered scroll-horizontal"id="table_id"> <thead><tr> <!--<th>#</th>--> <th class="text-center">Vendedor</th><th class="text-center">Venta</th> <th class="text-center">Meta</th><th class="text-center">Diferencia</th><th class="text-center"> Cumplimiento</th><th class="text-center">Detalles</th> </tr></thead><tbody id="tbodytable1ID"></tbody></table></div> </div></div></div>'
+
+function showData() {
+  let tableHtml = '';
+  let i = 0;
+  jsonData.forEach(todo => {
+    let cump = cumpl(todo.infoResult.data[0].sale, todo.infoResult.data[0].budget);
+    let style = cump < 80 ? 'background-color: #f8696963!important;' : cump < 100 ?  'background-color: #f8d76963 !important;' : 'background-color: #a2f86963 !important;'   ;
+    todo["id"] = i++;
+    tableHtml += '<tr id = ' + todo.id + '>';
+    tableHtml += '<td class = "text-truncate text-center"' + '> ' + todo.infoResult.data[0].slpName + '</td>';
+    tableHtml += '<td class = "text-truncate text-center"' + '>₡' + (todo.infoResult.data[0].sale).toLocaleString('en-emodeng') + '</td>';
+    tableHtml += '<td class = "text-truncate text-center"' + '>₡' + (todo.infoResult.data[0].budget).toLocaleString('en-emodeng') + '</td>';
+    tableHtml += '<td class = "text-truncate text-center"' + '>' + res(todo.infoResult.data[0].sale, todo.infoResult.data[0].budget) + '</td>';
+    tableHtml += '<td class = "text-truncate text-center"> <span class="badge badge-light"  style="'+style+'"' + '>' +cump + '%' + '</span></td>';
+    tableHtml += '<td class = "text-truncate text-center" ' + '><a href="dashboard.html"  onClick="actualClient(' + todo.id + ')"><i class="fas fa-paper-plane"></i></a></td>';
+    tableHtml += '</tr>';
+
+  });
+  document.getElementById("tabla").innerHTML = table;
+
+  document.getElementById("tbodytable1ID").innerHTML = tableHtml;
+
+  console.log(jsonData);
+  console.log(document.getElementById('demo'));
+
+   $('#table_id').DataTable({
+    "aaSorting": [],
+    "paging": true,
+    "lengthMenu": [3, 5, 9],
+    "pageLength": 3
+  });
+
+  $('#table_id').on("dblclick",  'tr', function () {
+    actualClient( this.id);
+    window.location.replace('dashboard.html');
+    } );
 }
 
-
-function getData (){
-    rootRef.on('value',(snap)=>{
-    jsonData = snap.val();
-    console.log(jsonData);
+function getData() {
+  return new Promise((resolve, reject) => {
+     setTimeout(
+      ()=>{
+          rootRef.on('value',(snap)=>{
+            resolve (snap.val());
+          });
+        },
+        1500
+    );
   });
 }
 
 
-let actualClient = element => localStorage.setItem("data",JSON.stringify(jsonData.filter(k => k.id==element)));
+async function startTable() {
+  await getData()
+    .then(
+      json => {
+        jsonData = json;
+        showData();
+      }
+    )
+    .catch(error => {
+      console.log(error)
+    });
+}
+
+$(document).ready(function(){
+  startTable();
+});
+$(window).on("load",function(){
+  $(".loader-container").fadeOut(1000);
+});
+
+
+let actualClient = element => localStorage.setItem("data", JSON.stringify(jsonData.filter(k => k.id == element)));
+
+
